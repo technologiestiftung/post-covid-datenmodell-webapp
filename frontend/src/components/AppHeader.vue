312881fdc5c92cdc7753a8adbce57cdca813227f
jjmllr <68501961+jjmllr@users.noreply.github.com>
2025-02-21 18:48:24 +0100

    Update styling

--- frontend/src/components/AppHeader.vue
+++ frontend/src/components/AppHeader.vue
@@ -1,48 +1,35 @@
 <template>
   <v-app-bar :elevation="0">
     <v-container>
-      <v-row justify="center">
-        <v-col 
-          cols="12" 
-          lg="10" 
-          xl="9" 
-          xxl="7"
-        >          
-          <div class="d-flex align-center justify-space-between pt-3 pb-2">
-            <div class="ml-2">
-              <img
-                src="../assets/header-logo.svg"
-                height="60px"
-                width="60px"
-              > 
-            </div>
-            <div class="d-flex flex-wrap gap-2 align-center">
-              <v-btn 
-                variant="flat" 
-                size="small" 
-                class="text-primary text-capitalize"                    
-              >
-                Metadatenbank
-              </v-btn>
-              <v-btn 
-                variant="flat" 
-                size="small" 
-                class="text-primary text-capitalize"
-              >
-                Daten vorschlagen
-              </v-btn>
-              <v-btn 
-                variant="outlined"
-                rounded="xl" 
-                size="small" 
-                class="text-primary text-capitalize"                    
-              >
-                Datensatz-Export
-              </v-btn>                  
-            </div>
-          </div>
-        </v-col>        
-      </v-row>
-    </v-container>         
+      <div class="d-flex align-center justify-space-between pt-3 pb-2">
+        <div class="ml-2">
+          <img src="../assets/header-logo.svg" height="60px" width="60px" />
+        </div>
+        <div class="d-flex flex-wrap gap-2 align-center">
+          <v-btn
+            variant="flat"
+            size="small"
+            class="text-primary text-capitalize"
+          >
+            Metadatenbank
+          </v-btn>
+          <v-btn
+            variant="flat"
+            size="small"
+            class="text-primary text-capitalize"
+          >
+            Daten vorschlagen
+          </v-btn>
+          <v-btn
+            variant="outlined"
+            rounded="xl"
+            size="small"
+            class="text-primary text-capitalize"
+          >
+            Datensatz-Export
+          </v-btn>
+        </div>
+      </div>
+    </v-container>
   </v-app-bar>
 </template>
--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -1,169 +1,157 @@
 <template>
-  <div class="d-flex flex-column remove-top-margin">
+  <div class="banner-section d-flex flex-column pa-15">
     <v-container>
-      <v-row
-        justify="center"
-        :class="{
-          'remove-left-right-margin-xs': $vuetify.display.xs,
-          'remove-left-right-margin-lg': $vuetify.display.lg,
-          'remove-left-right-margin': $vuetify.display.xxl,
-        }"
-      >
-        <v-col cols="12" md="11" lg="12" xl="10" xxl="8">
-          <div class="banner-section d-flex flex-column pa-15">
-            <h1 class="banner-title mt-15 text-h4">
-              Finden Sie genau die Datensätze, die Ihre Forschung mit MII-Daten
-              voranbringen.
-            </h1>
-            <p class="banner-subtitle mt-10">
-              Dieser Metadatentkatalog hilft Ihnen, offene Datensätze zu finden,
-              die perfekt mit MII-Daten kompatibel sind. Mit flexiblen Filtern
-              und Downloads im richtigen Format sparen Sie wertvolle Zeit bei
-              der Datenvorbereitung.
-            </p>
-          </div>
-        </v-col>
-      </v-row>
-      <v-row justify="center" class="mt-0">
-        <v-col cols="12" lg="12" xl="8" xxl="8" class="bg-home-surface">
-          <div class="d-flex flex-column align-center pt-5">
-            <v-text-field
-              variant="solo"
-              rounded="xl"
-              placeholder="Welche Datensätze suchen Sie?"
-              hide-details
-              clearable
-              width="70%"
-              density="compact"
-              prepend-inner-icon="mdi-magnify"
-              append-inner-icon="mdi-arrow-right-circle"
-            />
-
-            <div class="d-flex mt-2 flex-wrap justify-center">
-              <v-btn
-                class="ma-1 text-capitalize text-primary text-body-2"
-                variant="outlined"
-                rounded="xl"
-                density="compact"
-              >
-                Rehabilitation
-              </v-btn>
-
-              <v-btn
-                class="ma-1 text-capitalize text-primary text-body-2"
-                variant="outlined"
-                rounded="xl"
-                density="compact"
-              >
-                Wetter
-              </v-btn>
-
-              <v-btn
-                class="ma-1 text-capitalize text-primary text-body-2"
-                variant="outlined"
-                rounded="xl"
-                density="compact"
-              >
-                Emissionen
-              </v-btn>
-
-              <v-btn
-                class="ma-1 text-capitalize text-primary text-body-2"
-                variant="outlined"
-                rounded="xl"
-                density="compact"
-              >
-                Sozialstruktur
-              </v-btn>
-
-              <v-btn
-                class="ma-1 text-capitalize text-primary text-body-2"
-                variant="outlined"
-                rounded="xl"
-                density="compact"
-              >
-                Alle Datensätze
-              </v-btn>
-            </div>
-          </div>
-        </v-col>
-      </v-row>
-      <v-row justify="center">
-        <v-col cols="12" lg="12" xl="9" xxl="8" class="bg-home-surface">
-          <p class="pl-10 ml-10 text-h4 mt-10">
-            Daten sortieren und für MII-Daten anpassen
-          </p>
-          <p class="pl-10 ml-10 text-body-1 mt-5">
-            Mit unserer Webanwendung können Sie sofort passende offene
-            Datensätze finden, die sich mit dem Kerndatensatz der
-            Medizinformatikinitative verschneiden lassen, zum Beispiel über
-            räumlichen oder zeitlichen Bezug oder über Altersgruppe und
-            Geschlecht. Die Anwendung erlaubt ebenfalls eine Filterung nach
-            Thema und bereitet die offenen Daten automatisch in einem
-            kompatiblen Format vor – bereit für eine nahtlose Integration mit
-            Ihren MII-Daten.
-          </p>
-          <p class="pl-10 ml-10 mt-12 font-weight-bold">
-            Filtern nach MII-Schlüsselattribute
-          </p>
-          <AttributeCategories class="pl-10 ml-10" />
-        </v-col>
-      </v-row>
-      <v-row justify="center">
-        <v-col cols="12" lg="12" xl="9" xxl="8" class="bg-home-white">
-          <p class="pl-10 ml-10 text-h4 mt-10">
-            Vier Datenwelten für umfassende Analysen
-          </p>
-          <p class="pl-10 ml-10 text-body-1 mt-5 mb-10">
-            Erkunden Sie, wie Umweltbedingungen, lokale Gesundheitsressourcen,
-            sozioökonomische Faktoren und demografische Merkmale das Leben und
-            die Erholung von Long-COVID-Betroffenen beeinflussen. Filtern und
-            sortieren Sie relevante Datensätze, um tiefere Einblicke zu gewinnen
-            und fundierte Antworten auf Ihre Forschungsfragen zu finden.
-          </p>
-        </v-col>
-      </v-row>
-      <v-row justify="center" class="mt-0">
-        <v-col cols="12" lg="6" xl="6" xxl="4" class="bg-home-white">
-          <HomeDataWorld
-            icon="umwelt"
-            title="Umwelt"
-            subtitle="Wenn Luftqualität zur Gesundheitsfrage wird"
-            description="Untersuche den Einfluss von Umweltfaktoren wie z.B. die Luftqualität auf Post-Covid-Verläufe. Gibt es zeitliche oder regionale Unterschiede? Sind bestimmte Personen- oder Altersgruppen stärker durch Umweltbelastungen gefährdet? Identifizieren Sie potenzielle Risikofaktoren."
-          />
-        </v-col>
-
-        <v-col cols="12" lg="6" xl="6" xxl="4" class="bg-home-white">
-          <HomeDataWorld
-            icon="gesundheit"
-            title="Gesundheit"
-            subtitle="Reha-Angebote für ein starkes Comeback"
-            description="Erkunden Sie die medizinische Versorgung in Deutschland. Welche und wie viele Reha- und Pflegeangebote stehen Post-Covid-Patienten in Ihrer Region zur Verfügung? Wie steht es um die Zugänglichkeit dieser Angebote für betroffene Patienten? Werfen Sie einen Blick auf die Gesundheits-Infrastruktur."
-          />
-        </v-col>
-      </v-row>
-
-      <v-row justify="center" class="mt-0">
-        <v-col cols="12" lg="6" xl="6" xxl="4" class="bg-home-white">
-          <HomeDataWorld
-            icon="gesellschaft"
-            title="Gesellschaft"
-            subtitle="Sozioökonomische Hürden im Blick"
-            description="Analysieren Sie den Zusammenhang zwischen sozioökonomischen Faktoren wie z. B. Bildungsniveau oder durchschnittliches Einkommen und Post-Covid. Wie wirken sich unterschiedliche Lebenssituationen auf das Risiko einer Post-Covid-Erkrankung und die Chancen auf eine erfolgreiche Genesung aus? Stellen Sie Hypothesen auf."
-          />
-        </v-col>
-
-        <v-col cols="12" lg="6" xl="6" xxl="4" class="bg-home-white">
-          <HomeDataWorld
-            icon="demografie"
-            title="Demografie"
-            subtitle="Jede Altersgruppe zählt: Gesundheit nach Maß"
-            description="Erforschen Sie Muster in den medizinischen Daten anhand demografischer Einflussfaktoren wie Alter und Geschlecht. Welche Schlüsse lassen sich in Bezug auf Prävention sowie bedarfsorientierte Behandlung und Genesung von Post-Covid-Patienten ableiten? Identifizieren Sie Risikogruppen."
-          />
-        </v-col>
-      </v-row>
+      <h1 class="banner-title mt-15 text-h4">
+        Finden Sie genau die Datensätze, die Ihre Forschung mit MII-Daten
+        voranbringen.
+      </h1>
+      <p class="banner-subtitle mt-10">
+        Dieser Metadatentkatalog hilft Ihnen, offene Datensätze zu finden, die
+        perfekt mit MII-Daten kompatibel sind. Mit flexiblen Filtern und
+        Downloads im richtigen Format sparen Sie wertvolle Zeit bei der
+        Datenvorbereitung.
+      </p>
     </v-container>
   </div>
+
+  <v-container>
+    <div class="d-flex flex-column align-center pt-5">
+      <v-text-field
+        variant="solo"
+        flat
+        bg-color="#EAEAF6"
+        rounded="xl"
+        placeholder="Welche Datensätze suchen Sie?"
+        hide-details
+        clearable
+        width="70%"
+        density="comfortable"
+        prepend-inner-icon="mdi-magnify"
+      >
+        <template #append-inner>
+          <v-btn icon variant="text" @click="() => console.log('search')">
+            <v-icon size="large" color="#ACACDA">mdi-arrow-right-circle</v-icon>
+          </v-btn>
+        </template>
+      </v-text-field>
+
+      <div class="d-flex mt-2 flex-wrap justify-center">
+        <v-btn
+          class="ma-1 text-capitalize text-primary text-body-2"
+          variant="outlined"
+          rounded="xl"
+          density="compact"
+        >
+          Rehabilitation
+        </v-btn>
+
+        <v-btn
+          class="ma-1 text-capitalize text-primary text-body-2"
+          variant="outlined"
+          rounded="xl"
+          density="compact"
+        >
+          Wetter
+        </v-btn>
+
+        <v-btn
+          class="ma-1 text-capitalize text-primary text-body-2"
+          variant="outlined"
+          rounded="xl"
+          density="compact"
+        >
+          Emissionen
+        </v-btn>
+
+        <v-btn
+          class="ma-1 text-capitalize text-primary text-body-2"
+          variant="outlined"
+          rounded="xl"
+          density="compact"
+        >
+          Sozialstruktur
+        </v-btn>
+
+        <v-btn
+          class="ma-1 text-capitalize text-primary text-body-2"
+          variant="outlined"
+          rounded="xl"
+          density="compact"
+        >
+          Alle Datensätze
+        </v-btn>
+      </div>
+    </div>
+    <p class="pl-10 ml-10 text-h4 mt-10">
+      Daten sortieren und für MII-Daten anpassen
+    </p>
+    <p class="pl-10 ml-10 text-body-1 mt-5">
+      Mit unserer Webanwendung können Sie sofort passende offene Datensätze
+      finden, die sich mit dem Kerndatensatz der Medizinformatikinitative
+      verschneiden lassen, zum Beispiel über räumlichen oder zeitlichen Bezug
+      oder über Altersgruppe und Geschlecht. Die Anwendung erlaubt ebenfalls
+      eine Filterung nach Thema und bereitet die offenen Daten automatisch in
+      einem kompatiblen Format vor – bereit für eine nahtlose Integration mit
+      Ihren MII-Daten.
+    </p>
+    <p class="pl-10 ml-10 mt-12 font-weight-bold">
+      Filtern nach MII-Schlüsselattribute
+    </p>
+    <AttributeCategories class="pl-10 ml-10" />
+  </v-container>
+  <v-row justify="center">
+    <v-col cols="12" lg="12" xl="9" xxl="8" class="bg-home-white">
+      <p class="pl-10 ml-10 text-h4 mt-10">
+        Vier Datenwelten für umfassende Analysen
+      </p>
+      <p class="pl-10 ml-10 text-body-1 mt-5 mb-10">
+        Erkunden Sie, wie Umweltbedingungen, lokale Gesundheitsressourcen,
+        sozioökonomische Faktoren und demografische Merkmale das Leben und die
+        Erholung von Long-COVID-Betroffenen beeinflussen. Filtern und sortieren
+        Sie relevante Datensätze, um tiefere Einblicke zu gewinnen und fundierte
+        Antworten auf Ihre Forschungsfragen zu finden.
+      </p>
+    </v-col>
+  </v-row>
+  <v-row justify="center" class="mt-0">
+    <v-col cols="12" lg="6" xl="6" xxl="4" class="bg-home-white">
+      <HomeDataWorld
+        icon="umwelt"
+        title="Umwelt"
+        subtitle="Wenn Luftqualität zur Gesundheitsfrage wird"
+        description="Untersuche den Einfluss von Umweltfaktoren wie z.B. die Luftqualität auf Post-Covid-Verläufe. Gibt es zeitliche oder regionale Unterschiede? Sind bestimmte Personen- oder Altersgruppen stärker durch Umweltbelastungen gefährdet? Identifizieren Sie potenzielle Risikofaktoren."
+      />
+    </v-col>
+
+    <v-col cols="12" lg="6" xl="6" xxl="4" class="bg-home-white">
+      <HomeDataWorld
+        icon="gesundheit"
+        title="Gesundheit"
+        subtitle="Reha-Angebote für ein starkes Comeback"
+        description="Erkunden Sie die medizinische Versorgung in Deutschland. Welche und wie viele Reha- und Pflegeangebote stehen Post-Covid-Patienten in Ihrer Region zur Verfügung? Wie steht es um die Zugänglichkeit dieser Angebote für betroffene Patienten? Werfen Sie einen Blick auf die Gesundheits-Infrastruktur."
+      />
+    </v-col>
+  </v-row>
+
+  <v-row justify="center" class="mt-0">
+    <v-col cols="12" lg="6" xl="6" xxl="4" class="bg-home-white">
+      <HomeDataWorld
+        icon="gesellschaft"
+        title="Gesellschaft"
+        subtitle="Sozioökonomische Hürden im Blick"
+        description="Analysieren Sie den Zusammenhang zwischen sozioökonomischen Faktoren wie z. B. Bildungsniveau oder durchschnittliches Einkommen und Post-Covid. Wie wirken sich unterschiedliche Lebenssituationen auf das Risiko einer Post-Covid-Erkrankung und die Chancen auf eine erfolgreiche Genesung aus? Stellen Sie Hypothesen auf."
+      />
+    </v-col>
+
+    <v-col cols="12" lg="6" xl="6" xxl="4" class="bg-home-white">
+      <HomeDataWorld
+        icon="demografie"
+        title="Demografie"
+        subtitle="Jede Altersgruppe zählt: Gesundheit nach Maß"
+        description="Erforschen Sie Muster in den medizinischen Daten anhand demografischer Einflussfaktoren wie Alter und Geschlecht. Welche Schlüsse lassen sich in Bezug auf Prävention sowie bedarfsorientierte Behandlung und Genesung von Post-Covid-Patienten ableiten? Identifizieren Sie Risikogruppen."
+      />
+    </v-col>
+  </v-row>
 </template>
 
 <script setup lang="ts">
